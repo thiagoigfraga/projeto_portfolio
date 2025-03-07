a9e854ecafda462c9f4ad4cf780a5598f3f0d31b
Thiago Garcia <thiagogarciafraga99@gmail.com>
2025-03-07 19:10:49 -0300

    Adição de react

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,10 @@ import sitemap from '@astrojs/sitemap';
 import sentry from '@sentry/astro';
 import spotlightjs from '@spotlightjs/astro';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), sentry(), spotlightjs()],
+    integrations: [mdx(), sitemap(), sentry(), spotlightjs(), react()],
 });
