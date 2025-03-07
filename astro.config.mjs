6b74bdd42c742169a18e7f80a584988865d6a440
Thiago Garcia <thiagogarciafraga99@gmail.com>
2025-03-07 19:37:08 -0300

    Algumas modificações nas páginas e estilos para começar a trabalhar

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,14 @@ import spotlightjs from '@spotlightjs/astro';
 
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), sentry(), spotlightjs(), react()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), sentry(), spotlightjs(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
