7d32330204bbefc59a192c6c262265c5f4819548
Thiago Garcia <thiagogarciafraga99@gmail.com>
2025-03-07 18:59:21 -0300

    Adição de @sentry e @spotlightjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), sentry(), spotlightjs()],
 });
