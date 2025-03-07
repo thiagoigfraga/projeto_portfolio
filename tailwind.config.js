7a6868e42925630dd9c4a86035ebdba8fa309993
Thiago Garcia <thiagogarciafraga99@gmail.com>
2025-03-07 20:32:14 -0300

    Adição de mais personalizações e montando a call-out-box

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	theme: {
+		colors: {
+			indigo: {
+				// theme('colors.indigo.light')
+				light: "#b3bcf5",
+
+				// theme('colors.indigo.DEFAULT')
+				DEFAULT: "#5c6ac4",
+			},
+
+			// theme('colors.indigo-dark')
+			"indigo-dark": "#202e78",
+		},
+	},
+};
